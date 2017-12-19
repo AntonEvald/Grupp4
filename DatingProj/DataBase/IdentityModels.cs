281154ca142eb8d67e7355cf4ffc46d4b3aa870c
AndreasDaun <Andreasdaun@hotmail.com>
2017-12-19 12:54:47 +0100

    created message class

--- DatingProj/DataBase/IdentityModels.cs
+++ DatingProj/DataBase/IdentityModels.cs
@@ -3,12 +3,18 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.Collections;
 
 namespace DatingProj.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        
+        public virtual ICollection<Messeges> mess { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
