b612068092d1fdc27b8b35c5dfdbc96b031af7e0
Anton Evald <anton.evald@gmail.com>
2017-12-19 12:55:36 +0100

    _

---
+++ DatingProj/DataBase/messages.cs
@@ -0,0 +1,12 @@
+﻿namespace DatingProj.Models
+{
+    public class messages
+    {
+
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public virtual  ApplicationUser FromUser { get; set; }
+        public virtual ApplicationUser ToUser { get; set; }
+
+    }
+}
