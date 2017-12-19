8faf7413afc2a8c93aa3d96aad307a4690d2a0f7
Anton Evald <anton.evald@gmail.com>
2017-12-19 13:47:01 +0100

    :)

---
+++ DatingProj/DataBase/Models/FriendRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace DatingProj.Models
+{
+    public class FriendRequest
+    {
+        public int Id { get; set; }
+        public ApplicationUser ToUser { get; set; }
+        public ApplicationUser FromUser { get; set; }
+        public virtual ApplicationUser 
+    }
+}
