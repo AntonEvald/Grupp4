d3ad0e68782641d0c0159fc5910bc0c07e49f4f4
Andreas Daun <AndreasDaun@hotmail.com>
2017-12-20 09:23:26 +0100

    Edit profile

--- DatingProj/DatingProj/Controllers/ProfilController.cs
+++ DatingProj/DatingProj/Controllers/ProfilController.cs
@@ -5,25 +5,49 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using DatingProj.Models;
+using Microsoft.Ajax.Utilities;
 using Microsoft.AspNet.Identity;
 
 namespace DatingProj.Controllers
 {
-    public class ProfilController : Controller
+    public class ProfilController : BaseController
     {
+
+    
+
         // GET: Profil
         public ActionResult Index()
         {
-            using (var db = new ApplicationDbContext())
-            {
+          
+            
                 var userName = User.Identity.Name;
 
                 var user = db.Users.Single(x => x.UserName == userName);
                 return View(user);
-            }
+            
         }
 
-       
+        public ActionResult Edit()
+        {
+           
+                var userName = User.Identity.Name;
 
+                var user = db.Users.Single(x => x.UserName == userName);
+                return View(user);
+            
+        }
+        [HttpPost]
+        public ActionResult Edit(ApplicationUser user)
+        {
+           
+                var userName = User.Identity.Name;
+                var edit = db.Users.Single(x => x.UserName == userName);
+                if (TryUpdateModel(edit, "", new string[] {"UserName", "Name", "Description"}))
+                {
+                    db.SaveChanges();
+                }
+                return View();
+            
+        }
     }
 }
