b1936552fffe1b8b88342605bdcc22b16a51e73c
Philip Karlsson <philip96@hotmail.com>
2017-12-21 16:38:45 +0100

    ?!?!?!

--- DatingProj/DatingProj/Controllers/UsersController.cs
+++ DatingProj/DatingProj/Controllers/UsersController.cs
@@ -11,7 +11,6 @@ namespace DatingProj.Controllers
     public class UsersController : BaseController
     {
         // GET: Users
-        
         public ActionResult Index(string searchString)
         {
             var users = db.Users.ToList();
@@ -21,7 +20,7 @@ namespace DatingProj.Controllers
                     List<Models.ApplicationUser> filteredList = users.Where(User => User.Name.Contains(searchString)).Cast<Models.ApplicationUser>().ToList();
                     return View(filteredList);
                 }
-            return View(users);
+            return View(users.ToList());
         }
         public ActionResult UserProfil(string id)
         {
