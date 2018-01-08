bb8998f0bffff0d063aa96c0eb368e17c912d141
Anton Evald <anton.evald@gmail.com>
2018-01-08 12:12:31 +0100

    Den inloggade användaren listas inte i listan i find users och lagt till lite fler bilder för botar

--- DatingProj/DataBase/Models/ApplicationDbContext.cs
+++ DatingProj/DataBase/Models/ApplicationDbContext.cs
@@ -49,20 +49,26 @@ namespace DataBase.Models
         protected override void Seed(ApplicationDbContext context)
         {
             string path = Path.Combine(HttpRuntime.AppDomainAppPath, @"Images/noavatar.png");
+            string path2 = Path.Combine(HttpRuntime.AppDomainAppPath, @"Images/noavatar2.png");
+            string path3 = Path.Combine(HttpRuntime.AppDomainAppPath, @"Images/noavatar3.png");
+            string path4 = Path.Combine(HttpRuntime.AppDomainAppPath, @"Images/noavatar4.png");
             var pic = File.ReadAllBytes(path);
+            var pic2 = File.ReadAllBytes(path2);
+            var pic3 = File.ReadAllBytes(path3);
+            var pic4 = File.ReadAllBytes(path4);
             var store = new UserStore<ApplicationUser>(context);
             var userManager = new ApplicationUserManager(store);
             var user1 = new ApplicationUser { Name = "Admin Adminsson", UserName = "admin@mail.com", Email = "admin@mail.com", UserPhoto = pic, Searchable = true};
-            var user2 = new ApplicationUser { Name = "Sven Svensson", UserName = "sven@mail.com", Email = "sven@mail.com", UserPhoto = pic , Searchable = true};
-            var user3 = new ApplicationUser { Name = "Johan Johansson", UserName = "johan@mail.com", Email = "johan@mail.com", UserPhoto = pic, Searchable = true};
-            var user4 = new ApplicationUser { Name = "Admin Adminsson", UserName = "ja@mail.com", Email = "ja@mail.com", UserPhoto = pic, Searchable = true };
-            var user5 = new ApplicationUser { Name = "Admin Adminsson", UserName = "min@mail.com", Email = "min@mail.com", UserPhoto = pic, Searchable = true };
-            var user6 = new ApplicationUser { Name = "Admin Adminsson", UserName = "adin@mail.com", Email = "adin@mail.com", UserPhoto = pic, Searchable = true };
-            var user7 = new ApplicationUser { Name = "Admin Adminsson", UserName = "admn@mail.com", Email = "admn@mail.com", UserPhoto = pic, Searchable = true };
-            var user8 = new ApplicationUser { Name = "Admin Adminsson", UserName = "ada@mail.com", Email = "ada@mail.com", UserPhoto = pic, Searchable = true };
-            var user9 = new ApplicationUser { Name = "Admin Adminsson", UserName = "adn@mail.com", Email = "adn@mail.com", UserPhoto = pic, Searchable = true };
-            var user10 = new ApplicationUser { Name = "Admin Adminsson", UserName = "asd@mail.com", Email = "asd@mail.com", UserPhoto = pic, Searchable = true };
-            var user11 = new ApplicationUser { Name = "Admin Adminsson", UserName = "asdf@mail.com", Email = "asdf@mail.com", UserPhoto = pic, Searchable = true };
+            var user2 = new ApplicationUser { Name = "Sven Svensson", UserName = "sven@mail.com", Email = "sven@mail.com", UserPhoto = pic2 , Searchable = true};
+            var user3 = new ApplicationUser { Name = "Johan Johansson", UserName = "johan@mail.com", Email = "johan@mail.com", UserPhoto = pic3, Searchable = true};
+            var user4 = new ApplicationUser { Name = "Anna Andersson", UserName = "ja@mail.com", Email = "ja@mail.com", UserPhoto = pic4, Searchable = true };
+            var user5 = new ApplicationUser { Name = "Johanna Johansson", UserName = "min@mail.com", Email = "min@mail.com", UserPhoto = pic, Searchable = true };
+            var user6 = new ApplicationUser { Name = "Snygg Kille", UserName = "adin@mail.com", Email = "adin@mail.com", UserPhoto = pic2, Searchable = true };
+            var user7 = new ApplicationUser { Name = "Snygg Tjej", UserName = "admn@mail.com", Email = "admn@mail.com", UserPhoto = pic3, Searchable = true };
+            var user8 = new ApplicationUser { Name = "Snygg Odefinerad", UserName = "ada@mail.com", Email = "ada@mail.com", UserPhoto = pic4, Searchable = true };
+            var user9 = new ApplicationUser { Name = "Inte Bot", UserName = "adn@mail.com", Email = "adn@mail.com", UserPhoto = pic, Searchable = true };
+            var user10 = new ApplicationUser { Name = "Boten Anna", UserName = "asd@mail.com", Email = "asd@mail.com", UserPhoto = pic2, Searchable = true };
+            var user11 = new ApplicationUser { Name = "Göran Görsomhanvill", UserName = "asdf@mail.com", Email = "asdf@mail.com", UserPhoto = pic3, Searchable = true };
             userManager.CreateAsync(user1, "Asd123!").Wait();
             userManager.CreateAsync(user2, "Asd123!").Wait();
             userManager.CreateAsync(user3, "Asd123!").Wait();
--- DatingProj/DataBase/Models/IdentityModels.cs
+++ DatingProj/DataBase/Models/IdentityModels.cs
@@ -20,7 +20,6 @@ namespace DatingProj.Models
         public string Description { get; set; }
         public byte[] UserPhoto { get; set; }
         public bool Searchable { get; set; }
-
         public virtual ICollection<Posts> posts { get; set; }
         public virtual ICollection<Friend> Senders { get; set; }
         public virtual ICollection<Friend> Receivers { get; set; }
